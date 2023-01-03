cd5fe1045b9b1efbe3eaba8630c3f3aeffc1238c
Aleksieiev-Valentyn <director@druk.kh.ua>
2023-01-03 15:03:29 +0200

    Day 2 - 1.3

--- service/service/urls.py
+++ service/service/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import routers
 from django.contrib import admin
 from django.urls import path
+
+from services.views import SubscriptionView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+router = routers.DefaultRouter()
+router.register(r'api/subscriptions', SubscriptionView)
+
+urlpatterns += router.urls
+print(urlpatterns)
---
+++ service/services/serializers.py
@@ -0,0 +1,11 @@
+from services.models import Subscription
+from rest_framework import serializers
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    client_name = serializers.CharField(source='client.company_name')
+    email = serializers.CharField(source='client.user.email')
+
+    class Meta:
+        model = Subscription
+        fields = ('id', 'plan_id', 'client_name', 'email')
