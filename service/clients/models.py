b003b18653475ecbb1ed27ca89afbfff81bf5da2
Aleksieiev-Valentyn <director@druk.kh.ua>
2023-01-03 14:46:14 +0200

    Day 2 - 1.2

--- service/clients/models.py
+++ service/clients/models.py
@@ -5,3 +5,6 @@ class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     company_name = models.CharField(max_length=100)
     full_adress = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Client: {self.company_name}"
--- service/services/models.py
+++ service/services/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
 
-from service.clients.models import Client
+from clients.models import Client
 
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Name: {self.name}, Price: {self.full_price}"
 
 
 class Plan(models.Model):
@@ -22,7 +25,14 @@ class Plan(models.Model):
                                                        MaxValueValidator(100)
                                                    ])
 
+    def __str__(self):
+        return f"Plan: {self.plan_type}, discount: {self.discount_percent}"
+
+
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.client}, {self.service}, {self.plan}"
