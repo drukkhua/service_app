18ea0a8634c0e3001b042a22640738a5c21284d0
Aleksieiev-Valentyn <director@druk.kh.ua>
2023-01-03 13:54:24 +0200

    Day 2 - 1.0

---
+++ service/clients/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    company_name = models.CharField(max_length=100)
+    full_adress = models.CharField(max_length=100)
