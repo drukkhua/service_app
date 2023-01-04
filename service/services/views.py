f0be9753ae4671a11cf0817c87af15530a7415cb
Aleksieiev-Valentyn <director@druk.kh.ua>
2023-01-04 14:34:11 +0200

    Day 3 - 1.1

--- service/services/views.py
+++ service/services/views.py
@@ -6,11 +6,13 @@ from services.models import Subscription
 from services.serializers import SubscriptionSerializer
 from django.db.models import Prefetch
 
+
 class SubscriptionView(ReadOnlyModelViewSet):
     # стандартный prefetch вытягивает все поля всех моделей
     # queryset = Subscription.objects.all().prefetch_related('client__user')
     queryset = Subscription.objects.all().prefetch_related(
-        Prefetch('client', queryset=Client.objects.all().select_related('user')\
+        'plan',
+        Prefetch('client', queryset=Client.objects.all().select_related('user') \
                  .only('company_name', 'user__email'))
     )
     serializer_class = SubscriptionSerializer
