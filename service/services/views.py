e438450683eadbb1ff57c0e2555faf249efe4baa
Aleksieiev-Valentyn <director@druk.kh.ua>
2023-01-05 11:28:07 +0200

    Day 4 - 1.1 (annotate price)

--- service/services/views.py
+++ service/services/views.py
@@ -4,7 +4,7 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 from clients.models import Client
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
@@ -14,5 +14,6 @@ class SubscriptionView(ReadOnlyModelViewSet):
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user') \
                  .only('company_name', 'user__email'))
-    )
+    ).annotate(price=F('service__full_price') -
+                     F('service__full_price') * F('plan__discount_percent') / 100)
     serializer_class = SubscriptionSerializer
