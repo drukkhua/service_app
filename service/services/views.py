0adbfd30cd6d1aa93961ba1fb7905b2c4a9d9e2c
Aleksieiev-Valentyn <director@druk.kh.ua>
2023-01-05 12:19:46 +0200

    Day 4 - 1.5 (refactoring some spaces)

--- service/services/views.py
+++ service/services/views.py
@@ -1,18 +1,17 @@
 # from django.shortcuts import render
+from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
-from django.db.models import Prefetch, F, Sum
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    # стандартный prefetch вытягивает все поля всех моделей
-    # queryset = Subscription.objects.all().prefetch_related('client__user')
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
-        Prefetch('client', queryset=Client.objects.all().select_related('user') \
+        Prefetch('client',
+                 queryset=Client.objects.all().select_related('user')
                  .only('company_name', 'user__email'))
     ).annotate(price=F('service__full_price') -
                      F('service__full_price') * F('plan__discount_percent') / 100)
